196d3ebc74b317202beab256bb6a93af37ae8ae9
Rojas-Andres <andresfelipe200004@gmail.com>
2021-09-25 23:36:16 -0500

    Añadiendo faker al docker-compose

--- api/api.py
+++ api/api.py
@@ -1,7 +1,8 @@
 from flask import Flask, flash,session,jsonify
 from db import Session , engine,connection_db
 from flask_sqlalchemy import SQLAlchemy
-
+import requests
+import json
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = connection_db
@@ -14,6 +15,23 @@ from models import *
 @app.route('/')
 def hola():
     return jsonify({"mensaje":'holas'})
-
+@app.route('/crear_registros')
+def crear_registros():
+    print("entre ")
+    url = 'http://faker-service/datos'
+    response = requests.get(url)
+    response_json = json.loads(response.text)
+    '''
+    for i in range(response_json["datos"]):
+        #data = DataFaker(nombre=i["name"], nombre_compania=i["company_email"], ciudad=i["city"],direccion=i["address"],telefono=i["phone_number"])
+        #db.session.add(data)
+        #db.session.commit()
+        print(i)
+    '''
+    for i in response_json["datos"]:
+        data = DataFaker(nombre=i["name"], nombre_compania=i["company_email"], ciudad=i["city"],direccion=i["address"],telefono=i["phone_number"])
+        db.session.add(data)
+        db.session.commit()
+    return response_json
 if __name__ == '__main__':
    app.run(host='0.0.0.0', port=80, debug=True)
--- api/models.py
+++ api/models.py
@@ -8,6 +8,7 @@ class DataFaker(db.Model):
     __tablename__ = 'datafaker'
     id = db.Column(db.Integer,autoincrement=True , primary_key=True)
     nombre = db.Column(db.String(255),unique=True)
-    nombre_compania = db.Column(db.String(70),unique=True)
-    ciudad = db.Column(db.String(70),unique=True)
-    direccion = db.Column(db.String(70),unique=True)
+    nombre_compania = db.Column(db.String(100),unique=True)
+    ciudad = db.Column(db.String(120),unique=True)
+    direccion = db.Column(db.String(200),unique=True)
+    telefono = db.Column(db.String(40),unique=True)
